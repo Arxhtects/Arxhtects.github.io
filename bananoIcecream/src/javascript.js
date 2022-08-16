9b9e6d49fea7b70d5e927356f53c1c6f96217e6d
Archtects <scott.ch4mb3rs@gmail.com>
2022-08-16 13:25:50 +0100

    Add files via upload

--- bananoIcecream/src/javascript.js
+++ bananoIcecream/src/javascript.js
@@ -1,5 +1,5 @@
 // import multiformats from 'https://cdn.skypack.dev/multiformats';
-import { CID } from 'https://cdn.jsdelivr.net/npm/multiformats@9.7.1/cjs/src/index.min.js';
+import { CID } from 'https://cdn.jsdelivr.net/npm/multiformats@9.7.1/cjs/src/cid.js';
 
 $(document).ready(function() {
 const bananoJs = window.bananocoinBananojs;
