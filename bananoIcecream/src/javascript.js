2ae21c0a96322935f309c6e5d2587873a270e4bf
Archtects <scott.ch4mb3rs@gmail.com>
2022-08-16 16:00:34 +0100

    Add files via upload

--- bananoIcecream/src/javascript.js
+++ bananoIcecream/src/javascript.js
@@ -313,7 +313,7 @@ function buidPage(nftBlocks, nftMeta) {
     }
     for(let i = 0; i < nftList.length; i++) {
       console.log(nftList[i]);
-      $("#data > .wrapper").append("<div data-subtype='" + nftList[i][0].amount + "'>" + JSON.stringify(nftList[i]) + "</div>");
+      $("#data > .wrapper").append("<div>" + JSON.stringify(nftList[i]) + "</div>");
     }
     for(let i = 0; i < nftMeta.length; i++) {
       //console.log(nftBlocks[i][0].amount);
