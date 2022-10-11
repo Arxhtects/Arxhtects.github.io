c7091047c6e177ee1c9869edb061410b971105c6
Archtects <scott.ch4mb3rs@gmail.com>
2022-10-11 13:02:57 +0100

    Add files via upload

--- Lucid/js/script.js
+++ Lucid/js/script.js
@@ -1,2 +1,2 @@
-const web3 = new Web3(Web3.givenProvider);
-console.log(web3);
+import * as web3 from "https://cdn.skypack.dev/web3@1.8.0";
+import * as WalletconnectUtils from "https://cdn.skypack.dev/@walletconnect/utils@1.8.0";
