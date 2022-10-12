d68c37c01dc69c9d0ce974cff680523c7e8a4edb
Archtects <scott.ch4mb3rs@gmail.com>
2022-10-12 17:42:56 +0100

    Update script.js

--- Lucid/js/script.js
+++ Lucid/js/script.js
@@ -5,7 +5,7 @@ const web3 = new Web3(window.ethereum);
 
     async function getDetails() {
         const contract = new web3.eth.Contract(TokenInfo, "0x34Bc797F40Df0445c8429d485232874B15561728");
-        const walletAddress = "archtects.eth";//TODO Connect and get (ReadONLY)
+        const walletAddress = "0xa3C121b60C4219274EB6a0e2AfA539336ad7E35D";//TODO Connect and get (ReadONLY)
         contract.defaultAccounts = walletAddress;
         const lucidBalance = await contract.methods.balanceOf(walletAddress).call();
         console.log(lucidBalance);
