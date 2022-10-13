748f3bb4433e189d6173ffa98b8d782c98938085
Archtects <scott.ch4mb3rs@gmail.com>
2022-10-13 17:35:51 +0100

    Add files via upload

--- Lucid/assets/js/script.js
+++ Lucid/assets/js/script.js
@@ -3,9 +3,9 @@
     const web3 = new Web3(window.ethereum);
 
     let baselineURI = "https://ipfs.io/ipfs/bafybeibcfgitbgkmyxmmanvbcfn5tgn4nn2j7ouhhzvc6cib7vsp6tnaci/"
+    const contract = new web3.eth.Contract(TokenInfo, "0x34Bc797F40Df0445c8429d485232874B15561728");
 
     async function getDetails() {
-        const contract = new web3.eth.Contract(TokenInfo, "0x34Bc797F40Df0445c8429d485232874B15561728");
         const walletAddress = "0xa3C121b60C4219274EB6a0e2AfA539336ad7E35D"; //TODO Connect and get (ReadONLY)
         contract.defaultAccounts = walletAddress;
         const lucidBalance = await contract.methods.balanceOf(walletAddress).call();
