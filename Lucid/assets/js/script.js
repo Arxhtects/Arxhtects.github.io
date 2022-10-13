8653edb06c94c3a8bb6c22fb75d2dea54527dded
Archtects <scott.ch4mb3rs@gmail.com>
2022-10-13 16:46:32 +0100

    Update script.js

--- Lucid/assets/js/script.js
+++ Lucid/assets/js/script.js
@@ -1,17 +1,16 @@
-import TokenInfo from "./token_info.js"
+    import TokenInfo from "./token_info.js"
 
-const web3 = new Web3(window.ethereum);
+    const web3 = new Web3(window.ethereum);
 
+    let baselineURI = "https://ipfs.io/ipfs/bafybeibcfgitbgkmyxmmanvbcfn5tgn4nn2j7ouhhzvc6cib7vsp6tnaci/"
 
     async function getDetails() {
         const contract = new web3.eth.Contract(TokenInfo, "0x34Bc797F40Df0445c8429d485232874B15561728");
         const walletAddress = "0xa3C121b60C4219274EB6a0e2AfA539336ad7E35D"; //TODO Connect and get (ReadONLY)
         contract.defaultAccounts = walletAddress;
         const lucidBalance = await contract.methods.balanceOf(walletAddress).call();
-        // console.log(lucidBalance);
-        // for(let index = 0; index < lucidBalance; index++) {
 
-        for(let i = 1; i < 6; i++) {
+        for(let i = 1; i < 500; i++) {
             const matchaddress = await contract.methods.ownerOf(i).call();
             console.log(matchaddress);
         }
