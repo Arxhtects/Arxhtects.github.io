409ebf87776a6cfcbab9365b477881bc14cb873f
Archtects <scott.ch4mb3rs@gmail.com>
2022-10-13 15:50:00 +0100

    Add files via upload

---
+++ Lucid/assets/js/script.js
@@ -0,0 +1,27 @@
+import TokenInfo from "./token_info.js"
+
+const web3 = new Web3(window.ethereum);
+
+
+    async function getDetails() {
+        const contract = new web3.eth.Contract(TokenInfo, "0x34Bc797F40Df0445c8429d485232874B15561728");
+        const walletAddress = "0xa3C121b60C4219274EB6a0e2AfA539336ad7E35D"; //TODO Connect and get (ReadONLY)
+        contract.defaultAccounts = walletAddress;
+        const lucidBalance = await contract.methods.balanceOf(walletAddress).call();
+        // console.log(lucidBalance);
+        $.each(lucidBalance, function(index, val) {
+            const tokenId = await contract.methods.tokenOfOwnerByIndex(walletAddress, index);
+            
+            let tokenUri = await contract.methods.tokenURI(tokenId).call();
+            if(tokenUri.startsWith("ipfs://")) {
+                tokenUri = 'https://ipfs.io/ipfs/' + tokenUri.split("ipfs://")[1];
+            }
+
+            const tokenMeta = await fetch(tokenUri).then((response) => response.json);
+            console.log(tokenMeta);
+        });
+    }
+
+    $("#button").on("click", function() {
+        getDetails();
+    });
