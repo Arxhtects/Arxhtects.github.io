4ad181e229497028d2128bd94fc0c532e2d8dea4
Archtects <scott.ch4mb3rs@gmail.com>
2023-09-05 15:25:14 +0100

    Add files via upload

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,20 @@
 $(document).ready(function() {
-    console.log("updated 6");
+    console.log("updated 7");
     
-    $.getJSON( "template/projects.json", function( data ) {
+    $.getJSON( "https://raw.githubusercontent.com/Arxhtects/Arxhtects.github.io/main/template/projects.json", function( data ) {
         var items = [];
         $.each( data.project, function( key, val ) {
-          items.push( "<li id='" + key + "'>" + val + "</li>" );
+          items.push("<div id='" + key + "' class='projects'>");
+          items.push("<h3>" + val.projecttitle + "</h3>");
+          items.push("<ul class='hashtag-list'>");
+            $.each( data.project[key].hashtags, function( key, val ) {
+              items.push( "<li id='" + key + "'>" + val + "</li>" );
+            });
+          items.push("</ul>");
+          items.push("<figure><img src='https://raw.githubusercontent.com/Arxhtects/Arxhtects.github.io/main/template/project-banner/"+ val.image +"''></figure>");
+          items.push("</div>");
         });
        
-        $( "<ul/>", {
-          "class": "my-new-list",
-          html: items.join( "" )
-        }).appendTo( "body" );
+        $(items.join("")).appendTo( "body" );
       });
 });
