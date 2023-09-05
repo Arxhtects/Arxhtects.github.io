36e6040d222e6f228e044fe4282a310f35d30af7
Archtects <scott.ch4mb3rs@gmail.com>
2023-09-05 14:49:10 +0100

    Add files via upload

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,10 @@
 $(document).ready(function() {
-    console.log("updated 4");
+    console.log("updated 5");
     
     $.getJSON( "template/projects.json", function( data ) {
         var items = [];
         $.each( data, function( key, val ) {
-          items.push( "<li id='" + key.project_title + "'>" + val.project_title + "</li>" );
+          items.push( "<li id='" + key.project + "'>" + val.project + "</li>" );
         });
        
         $( "<ul/>", {
