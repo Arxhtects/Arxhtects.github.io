27ab569e831c20aa25cc588b72b259618ca51f31
Archtects <scott.ch4mb3rs@gmail.com>
2023-09-05 19:26:23 +0100

    Add files via upload

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,15 +4,15 @@ $(document).ready(function() {
     $.getJSON( "https://raw.githubusercontent.com/Arxhtects/Arxhtects.github.io/main/template/projects.json", function( data ) {
         var items = [];
         $.each( data.project, function( key, val ) {
-          items.push("<div id='" + key + "' class='projects'>");
+          items.push("<div id='project-id-" + key + "' class='projects'>");
           items.push("<h3>" + val.projecttitle + "</h3>");
           items.push("<ul class='hashtag-list'>");
             $.each( data.project[key].hashtags, function( key, val ) {
-              items.push( "<li id='" + key + "'>" + val + "</li>" );
+              items.push( "<li id='hashtag-id-" + key + "'>" + val + "</li>" );
             });
           items.push("</ul>");
           items.push("<figure style='background: " + val.color + "'><img src='https://raw.githubusercontent.com/Arxhtects/Arxhtects.github.io/main/template/project-banner/"+ val.image +"''></figure>");
-          items.push("<p>" + val.shortdescription + "</p>");
+          //items.push("<p>" + val.shortdescription + "</p>");
           items.push("</div>");
         });
        
